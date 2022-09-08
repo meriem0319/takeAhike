6239f41f879ef13e43404c52aab5e0baa1c20fc1
meriem0319 <meriem0319@gmail.com>
2022-09-08 16:28:15 -0400

    config done

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,38 @@
 const Sequelize = require("sequelize");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3008,
-  }
-);
+let sequelize;
 
-module.exports = sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3008,
+    }
+  );
+}
+
+const sessionConfig = {
+  secret: process.env.SECRET,
+  cookie: {
+    maxAge: 1 * 60 * 1000, // 1hour
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+module.exports = { sequelize, sessionConfig };
